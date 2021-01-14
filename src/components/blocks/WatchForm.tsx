36a780783d335363bba50c64fa96dd3609a64470
David Alvarez Restrepo <dalvarezrestrepo@openwt.com>
2021-01-14 09:32:49 +0100

    Installed and configured prettier

--- src/components/blocks/WatchForm.tsx
+++ src/components/blocks/WatchForm.tsx
@@ -1,41 +1,43 @@
-import React, {useEffect, useMemo, useState} from "react"
-import {TWatchItem} from "~/src/types/TWatchItem";
-import {useHistory} from "react-router-dom"
-import {useWatchService} from "~/src/components/hooks/useWatchService";
-import {Button} from "antd";
-import {selectFileOfType} from "@bytesoftio/helpers-input";
-import {useFileService} from "~/src/components/hooks/useFileService";
-import {ImagePreview} from "~/src/components/blocks/ImagePreview";
-import {WATCH_COLLECTION} from "~/src/const/routeNames";
-import {ImageSelector} from "~/src/components/blocks/ImageSelector";
+import React, { useMemo, useState } from "react"
+import { TWatchItem } from "~/src/types/TWatchItem"
+import { useHistory } from "react-router-dom"
+import { useWatchService } from "~/src/components/hooks/useWatchService"
+import { useFileService } from "~/src/components/hooks/useFileService"
+import { WATCH_COLLECTION } from "~/src/const/routeNames"
+import { ImageSelector } from "~/src/components/blocks/ImageSelector"
 
 interface PropsType {
-    watch?: TWatchItem,
+    watch?: TWatchItem
 }
 
 const initialFormState: TWatchItem = {
     uuid: null,
-    brand: '',
-    model: '',
-    description: '',
-    priceBought: '',
+    brand: "",
+    model: "",
+    description: "",
+    priceBought: "",
     image: null,
 }
 
-
-export const WatchForm = ({watch}: PropsType) => {
+export const WatchForm = ({ watch }: PropsType) => {
     const watchService = useWatchService()
     const fileService = useFileService()
     const history = useHistory()
     const [state, setState] = useState<TWatchItem>(watch ?? initialFormState)
 
     // Memo prevents the expensive calculation when we type in a field (which causes a render)
-    const image = useMemo(() => fileService.dataUrlToFileObject(state.image, 'watch'), [state.image])
+    const image = useMemo(
+        () => fileService.dataUrlToFileObject(state.image, "watch"),
+        [state.image]
+    )
 
-    const handleChange = (key: keyof typeof initialFormState, value: string) => {
+    const handleChange = (
+        key: keyof typeof initialFormState,
+        value: string
+    ) => {
         setState({
             ...state,
-            ...{[key]: value}
+            ...{ [key]: value },
         })
     }
 
@@ -52,10 +54,10 @@ export const WatchForm = ({watch}: PropsType) => {
     }
 
     const onImageChange = async (file) => {
-        const base64File: string = await fileService.toBase64(file) as any
+        const base64File: string = (await fileService.toBase64(file)) as any
         setState({
             ...state,
-            ...{image: base64File}
+            ...{ image: base64File },
         })
     }
 
@@ -63,8 +65,8 @@ export const WatchForm = ({watch}: PropsType) => {
 
     return (
         <div className="watch-collection-form">
-            <h1>{watch ? 'Edit' : 'Add'} a watch</h1>
-            <br/>
+            <h1>{watch ? "Edit" : "Add"} a watch</h1>
+            <br />
 
             <label>
                 Brand
@@ -74,7 +76,7 @@ export const WatchForm = ({watch}: PropsType) => {
                     onChange={(evt) => handleChange("brand", evt.target.value)}
                 />
             </label>
-            <br/>
+            <br />
 
             <label>
                 Model
@@ -84,43 +86,39 @@ export const WatchForm = ({watch}: PropsType) => {
                     onChange={(evt) => handleChange("model", evt.target.value)}
                 />
             </label>
-            <br/>
+            <br />
 
             <label>
                 Description
                 <input
                     type="text"
                     value={state.description}
-                    onChange={(evt) => handleChange("description", evt.target.value)}
+                    onChange={(evt) =>
+                        handleChange("description", evt.target.value)
+                    }
                 />
             </label>
-            <br/>
+            <br />
 
             <label>
                 Price bought
                 <input
                     type="number"
                     value={state.priceBought}
-                    onChange={(evt) => handleChange("priceBought", evt.target.value)}
+                    onChange={(evt) =>
+                        handleChange("priceBought", evt.target.value)
+                    }
                 />
             </label>
-            <br/>
+            <br />
 
-            <ImageSelector
-                image={image}
-                onImageChange={onImageChange}
-            />
+            <ImageSelector image={image} onImageChange={onImageChange} />
 
-            <br/>
-            <br/>
-            <button
-                onClick={() => watch ? updateWatch() : addWatch()}
-            >
+            <br />
+            <br />
+            <button onClick={() => (watch ? updateWatch() : addWatch())}>
                 Submit
             </button>
-
         </div>
-
     )
-
 }
