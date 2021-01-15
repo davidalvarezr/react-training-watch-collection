5c2a116e7557bc3e8d70b710f9170acab0a8ebf1
David Alvarez Restrepo <dalvarezrestrepo@openwt.com>
2021-01-15 13:45:20 +0100

    Simplified MenuHeader

--- src/components/blocks/MenuHeader.tsx
+++ src/components/blocks/MenuHeader.tsx
@@ -1,16 +1,14 @@
-import { Menu } from "antd"
-import { links } from "~/src/config/links"
 import React from "react"
-import { Layout } from "antd"
+
+import { links } from "~/src/config/links"
 import { Link, useLocation } from "react-router-dom"
-import { useConsoleService } from "~/src/components/hooks/useConsoleService"
+import { Layout, Menu } from "antd"
 
 const { Header } = Layout
+const { Item } = Menu
 
 export const MenuHeader = () => {
     const { pathname } = useLocation()
-    const consoleService = useConsoleService()
-    consoleService.log("location", location)
 
     return (
         <Header>
@@ -18,21 +16,21 @@ export const MenuHeader = () => {
                 <h1>My Watch Collection</h1>
             </div>
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[pathname]}>
-                <Menu.Item key={links.home()}>
+                <Item key={links.home()}>
                     <Link to="/">Home</Link>
-                </Menu.Item>
-                <Menu.Item key={links.watchCollection()}>
+                </Item>
+                <Item key={links.watchCollection()}>
                     <Link to={links.watchCollection()}>Watch collection</Link>
-                </Menu.Item>
-                <Menu.Item key="/wish-list">
+                </Item>
+                <Item key="/wish-list">
                     <Link to="/wish-list">Wish list</Link>
-                </Menu.Item>
-                <Menu.Item key={links.currentTime()}>
+                </Item>
+                <Item key={links.currentTime()}>
                     <Link to={links.currentTime()}>Current Time</Link>
-                </Menu.Item>
-                <Menu.Item key={links.settings()}>
+                </Item>
+                <Item key={links.settings()}>
                     <Link to={links.settings()}>Settings</Link>
-                </Menu.Item>
+                </Item>
             </Menu>
         </Header>
     )
