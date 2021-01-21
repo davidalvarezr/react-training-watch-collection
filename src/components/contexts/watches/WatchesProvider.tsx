81c864ffd7739f215849b506dd0519520cfca615
David Alvarez Restrepo <dalvarezrestrepo@openwt.com>
2021-01-21 12:37:31 +0100

    [FIX] Reducer works correctly thanks to https://gist.github.com/astoilkov/013c513e33fe95fa8846348038d8fe42

--- src/components/contexts/watches/WatchesProvider.tsx
+++ src/components/contexts/watches/WatchesProvider.tsx
@@ -1,12 +1,9 @@
 import React, { Dispatch, ReactNode, useReducer } from "react"
 import { WatchesContext } from "./WatchesContext"
-import { WatchAction, WatchesAction } from "./actions"
+import { WatchAction } from "./actions"
 import { initialState, State } from "./state"
-import { useWatchService } from "~/src/components/hooks/useWatchService"
-import { Watch } from "~/src/types/Watch"
-import { v4 as uuidv4 } from "uuid"
-import { dropboxErrorMapper } from "~/src/services/error-mappers/dropboxErrorMapper"
-import { useConsoleService } from "~/src/components/hooks/useConsoleService"
+import { dispatchMiddleware, reducer } from "~/src/components/contexts/watches/reducer"
+import { Reducer } from "./reducer"
 
 export type Provider = {
     watches: State
@@ -14,113 +11,13 @@ export type Provider = {
 }
 
 type PropTypes = { children: ReactNode }
-export type Reducer = (state: State, action: WatchAction) => State
 
 export const WatchesProvider: React.FC<PropTypes> = ({ children }: PropTypes) => {
-    const watchService = useWatchService()
-    const consoleService = useConsoleService()
-
-    const [watches, dispatch] = useReducer<Reducer>((state, action) => {
-        consoleService.logAction(action, state)
-
-        switch (action.type) {
-            case WatchesAction.LOAD_FROM_LOCAL_STORAGE:
-                watchService
-                    .getWatchList()
-                    .then((watches) => {
-                        dispatch({
-                            type: WatchesAction.LOAD_FROM_LOCAL_STORAGE_SUCCESS,
-                            payload: watches,
-                        })
-                    })
-                    .catch((e) => {
-                        dispatch({
-                            type: WatchesAction.LOAD_FROM_LOCAL_STORAGE_FAILURE,
-                            payload: e,
-                        })
-                    })
-                return {
-                    ...state,
-                    localStorageRetrieveLoading: true,
-                    localStorageRetrieveError: undefined,
-                }
-            case WatchesAction.LOAD_FROM_LOCAL_STORAGE_SUCCESS:
-                return { ...state, watches: action.payload, localStorageRetrieveLoading: false }
-            case WatchesAction.LOAD_FROM_LOCAL_STORAGE_FAILURE:
-                return {
-                    ...state,
-                    initialized: true,
-                    localStorageRetrieveLoading: false,
-                    localStorageRetrieveError: action.payload,
-                }
-
-            case WatchesAction.CLEAR_LIST: {
-                const watches: Watch[] = []
-                watchService.setWatchList(watches)
-                return { ...state, watches }
-            }
-
-            case WatchesAction.ADD_WATCH: {
-                const watches: Watch[] = [{ ...action.payload, uuid: uuidv4() }, ...state.watches]
-                watchService.setWatchList(watches)
-                return { ...state, watches }
-            }
-
-            case WatchesAction.REMOVE_WATCH: {
-                const watches: Watch[] = state.watches.filter(
-                    (watch) => watch.uuid !== action.payload
-                )
-                watchService.setWatchList(watches)
-                return { ...state, watches }
-            }
-
-            case WatchesAction.UPDATE_WATCH: {
-                const { uuid, watch } = action.payload
-                const watches: Watch[] = state.watches.map((aWatch) =>
-                    aWatch.uuid === uuid ? { ...watch, uuid } : aWatch
-                )
-                watchService.setWatchList(watches)
-                return { ...state, watches }
-            }
-
-            case WatchesAction.UPLOAD:
-                watchService
-                    .sendListOnline()
-                    .then(() => dispatch({ type: WatchesAction.UPLOAD_SUCCESS }))
-                    .catch((e) => dispatch({ type: WatchesAction.UPLOAD_FAILURE, payload: e }))
-                return { ...state, uploading: true, uploadError: undefined }
-            case WatchesAction.UPLOAD_SUCCESS:
-                return { ...state, uploading: false }
-            case WatchesAction.UPLOAD_FAILURE:
-                return {
-                    ...state,
-                    uploading: false,
-                    uploadError: dropboxErrorMapper(action.payload),
-                }
-
-            case WatchesAction.DOWNLOAD:
-                watchService
-                    .persistWatchesFromCloud(action.payload)
-                    .then((watches) =>
-                        dispatch({ type: WatchesAction.DOWNLOAD_SUCCESS, payload: watches })
-                    )
-                    .catch((e) => dispatch({ type: WatchesAction.DOWNLOAD_FAILURE, payload: e }))
-                return { ...state, downloading: true, downloadError: undefined }
-            case WatchesAction.DOWNLOAD_SUCCESS:
-                return { ...state, downloading: false, watches: action.payload }
-            case WatchesAction.DOWNLOAD_FAILURE:
-                return {
-                    ...state,
-                    downloading: false,
-                    downloadError: dropboxErrorMapper(action.payload),
-                }
-
-            default:
-                return state
-        }
-    }, initialState)
+    const [watches, dispatch] = useReducer<Reducer>(reducer, initialState)
 
     return (
-        <WatchesContext.Provider value={{ watches, dispatch }}>{children}</WatchesContext.Provider>
+        <WatchesContext.Provider value={{ watches, dispatch: dispatchMiddleware(dispatch) }}>
+            {children}
+        </WatchesContext.Provider>
     )
 }
---
+++ src/components/contexts/watches/reducer.ts
@@ -0,0 +1,129 @@
+import React from "react"
+import { WatchAction, WatchesAction } from "~/src/components/contexts/watches/actions"
+import { Watch } from "~/src/types/Watch"
+import { dropboxErrorMapper } from "~/src/services/error-mappers/dropboxErrorMapper"
+import { consoleService, watchService } from "~/src/services/container"
+import { State } from "~/src/components/contexts/watches/state"
+import { v4 as uuidv4 } from "uuid"
+
+type DispatchReturn = (action: WatchAction) => void
+type DispatchMiddleware = (dispatch: React.Dispatch<WatchAction>) => DispatchReturn
+
+// Handles asynchronous dispatches
+export const dispatchMiddleware: DispatchMiddleware = (dispatch) => {
+    return (action) => {
+        switch (action.type) {
+            case WatchesAction.LOAD_FROM_LOCAL_STORAGE:
+                watchService
+                    .getWatchList()
+                    .then((watches) => {
+                        dispatch({
+                            type: WatchesAction.LOAD_FROM_LOCAL_STORAGE_SUCCESS,
+                            payload: watches,
+                        })
+                    })
+                    .catch((e) => {
+                        dispatch({
+                            type: WatchesAction.LOAD_FROM_LOCAL_STORAGE_FAILURE,
+                            payload: e,
+                        })
+                    })
+                return dispatch(action)
+
+            case WatchesAction.UPLOAD:
+                watchService
+                    .sendListOnline()
+                    .then(() => dispatch({ type: WatchesAction.UPLOAD_SUCCESS }))
+                    .catch((e) => dispatch({ type: WatchesAction.UPLOAD_FAILURE, payload: e }))
+                return dispatch(action)
+
+            case WatchesAction.DOWNLOAD:
+                watchService
+                    .persistWatchesFromCloud(action.payload)
+                    .then((watches) =>
+                        dispatch({ type: WatchesAction.DOWNLOAD_SUCCESS, payload: watches })
+                    )
+                    .catch((e) => dispatch({ type: WatchesAction.DOWNLOAD_FAILURE, payload: e }))
+                return dispatch(action)
+
+            default:
+                return dispatch(action)
+        }
+    }
+}
+
+export type Reducer = (state: State, action: WatchAction) => State
+
+export const reducer: Reducer = (state, action) => {
+    consoleService.logAction(action, state)
+
+    switch (action.type) {
+        case WatchesAction.LOAD_FROM_LOCAL_STORAGE:
+            return {
+                ...state,
+                localStorageRetrieveLoading: true,
+                localStorageRetrieveError: undefined,
+            }
+        case WatchesAction.LOAD_FROM_LOCAL_STORAGE_SUCCESS:
+            return { ...state, watches: action.payload, localStorageRetrieveLoading: false }
+        case WatchesAction.LOAD_FROM_LOCAL_STORAGE_FAILURE:
+            return {
+                ...state,
+                initialized: true,
+                localStorageRetrieveLoading: false,
+                localStorageRetrieveError: action.payload,
+            }
+
+        case WatchesAction.CLEAR_LIST: {
+            const watches: Watch[] = []
+            watchService.setWatchList(watches)
+            return { ...state, watches }
+        }
+
+        case WatchesAction.ADD_WATCH: {
+            const watches: Watch[] = [{ ...action.payload, uuid: uuidv4() }, ...state.watches]
+            watchService.setWatchList(watches)
+            return { ...state, watches }
+        }
+
+        case WatchesAction.REMOVE_WATCH: {
+            const watches: Watch[] = state.watches.filter((watch) => watch.uuid !== action.payload)
+            watchService.setWatchList(watches)
+            return { ...state, watches }
+        }
+
+        case WatchesAction.UPDATE_WATCH: {
+            const { uuid, watch } = action.payload
+            const watches: Watch[] = state.watches.map((aWatch) =>
+                aWatch.uuid === uuid ? { ...watch, uuid } : aWatch
+            )
+            watchService.setWatchList(watches)
+            return { ...state, watches }
+        }
+
+        case WatchesAction.UPLOAD:
+            return { ...state, uploading: true, uploadError: undefined }
+        case WatchesAction.UPLOAD_SUCCESS:
+            return { ...state, uploading: false }
+        case WatchesAction.UPLOAD_FAILURE:
+            return {
+                ...state,
+                uploading: false,
+                uploadError: dropboxErrorMapper(action.payload),
+            }
+
+        case WatchesAction.DOWNLOAD:
+            return { ...state, downloading: true, downloadError: undefined }
+        case WatchesAction.DOWNLOAD_SUCCESS:
+            return { ...state, downloading: false, watches: action.payload }
+        case WatchesAction.DOWNLOAD_FAILURE:
+            return {
+                ...state,
+                downloading: false,
+                downloadError: dropboxErrorMapper(action.payload),
+            }
+
+        default:
+            return state
+    }
+}
